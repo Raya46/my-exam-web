fb08a35f7564bf63dcdb4bb44bdf7bbb1d5d2d0c
rafaelrnzo <rl.lorenzo.256@gmail.com>
2024-03-22 20:37:40 +0700

    update UI admin : login,register,sidebar

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -35,7 +35,7 @@ const RegisterPage = () => {
       sekolah: sekolah
     });
 
-    if (response.data.data === 'berhasil') {
+    if (response.data.data === 'success') {
       navigate("/login");
       toast({
         title: "Register Berhasil",
@@ -54,59 +54,119 @@ const RegisterPage = () => {
   };
 
   return (
-    <Flex
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      bg="gray.200"
-    >
-      <Box bg="white" p={6} rounded="md" boxShadow="md" width="320px">
-        <Heading mb={6}>Register</Heading>
-        <form onSubmit={handleSubmit}>
-          <FormControl mb={4}>
-            <FormLabel htmlFor="name">name</FormLabel>
-            <Input
-              id="name"
-              type="name"
-              placeholder="name Anda"
-              value={name}
-              onChange={(e) => setname(e.target.value)}
-            />
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel htmlFor="sekolah">sekolah</FormLabel>
-            <Input
-              id="sekolah"
-              type="sekolah"
-              placeholder="sekolah Anda"
-              value={sekolah}
-              onChange={(e) => setSekolah(e.target.value)}
-            />
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <InputGroup>
-              <Input
+    <main className="flex h-screen w-full max-h-screen">
+      <div className="h-full w-full bg-white flex flex-col justify-between items-center p-4">
+        <div>
+          <p className="text-blue-800 font-bold text-2xl">ArohExam</p>
+        </div>
+        <div className="rounded-md border border-slate-200 p-8 justify-between flex w-2/4 flex-col items-center gap-8">
+          <p className="text-blue-800 font-semibold text-3xl">Sign In</p>
+          <form onSubmit={handleSubmit} className="w-full gap-3 flex flex-col">
+            <div className="w-full">
+              <input
+                id="sekolah"
+                type="sekolah"
+                value={sekolah}
+                onChange={(e) => setSekolah(e.target.value)}
+                className="border border-slate-200 rounded-md focus:border-blue-800 p-2 px-3 w-full focus:outline-none focus:ring-1 focus:ring-blue-800"
+                placeholder="School" />
+            </div>
+            <div className="w-full">
+              <input
+                id="name"
+                type="name"
+                value={name}
+                onChange={(e) => setname(e.target.value)}
+                className="border border-slate-200 rounded-md focus:border-blue-800 p-2 px-3 w-full focus:outline-none focus:ring-1 focus:ring-blue-800"
+                placeholder="Email" />
+            </div>
+            <div className="w-full">
+              <input
                 id="password"
-                type={showPassword ? "text" : "password"}
-                placeholder="Password Anda"
+                type="text"
+                // type={showPassword ? "text" : "password"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-              />
-              <InputRightElement>
-                <Button h="1.75rem" size="sm" onClick={handleShowPassword}>
-                  {showPassword ? <ViewOffIcon /> : <ViewIcon />}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
-          <Button colorScheme="blue" type="submit" width="100%">
-            Register
-          </Button>
-        </form>
-        <Text onClick={() => navigate('/login')}>Sudah punya akun? Login</Text>
-      </Box>
-    </Flex>
+                className="border border-slate-200 rounded-md focus:border-blue-800 p-2 px-3 w-full focus:outline-none focus:ring-1 focus:ring-blue-800"
+                placeholder="Password" />
+            </div>
+            <div className="w-full">
+              <button
+                type="submit"
+                className="border bg-blue-800 text-white rounded-md p-2 px-3 w-full "
+              >
+                Register
+              </button>
+            </div>
+          </form>
+          <div className="w-full flex justify-center ">
+            <p>Forgot password?<span className="p-1 text-blue-800 underline font-medium">Register</span></p>
+          </div>
+        </div>
+        <div>
+          <p>Already have an account?<button onClick={() => navigate('/login')} className="p-1 text-blue-800 underline font-medium">
+            login
+          </button></p>
+        </div>
+      </div>
+      {/* <div className="h-full w-2/4 md:block hidden">
+        <img src={imageLogin} className="w-full h-full object-cover" />
+      </div> */}
+
+    </main>
+    // <Flex
+    //   height="100vh"
+    //   alignItems="center"
+    //   justifyContent="center"
+    //   bg="gray.200"
+    // >
+    //   <Box bg="white" p={6} rounded="md" boxShadow="md" width="320px">
+    //     <Heading mb={6}>Register</Heading>
+    //     <form onSubmit={handleSubmit}>
+    //       <FormControl mb={4}>
+    //         <FormLabel htmlFor="name">name</FormLabel>
+    //         <Input
+    //           id="name"
+    //           type="name"
+    //           placeholder="name Anda"
+    //           value={name}
+    //           onChange={(e) => setname(e.target.value)}
+    //         />
+    //       </FormControl>
+    //       <FormControl mb={4}>
+    //         <FormLabel htmlFor="sekolah">sekolah</FormLabel>
+    //         <Input
+    //           id="sekolah"
+    //           type="sekolah"
+    //           placeholder="sekolah Anda"
+    //           value={sekolah}
+    //           onChange={(e) => setSekolah(e.target.value)}
+    //         />
+    //       </FormControl>
+    //       <FormControl mb={4}>
+    //         <FormLabel htmlFor="password">Password</FormLabel>
+    //         <InputGroup>
+    //           <Input
+    //             id="password"
+    //             type={showPassword ? "text" : "password"}
+    //             placeholder="Password Anda"
+    //             value={password}
+    //             onChange={(e) => setPassword(e.target.value)}
+    //           />
+    //           <InputRightElement>
+    //             <Button h="1.75rem" size="sm" onClick={handleShowPassword}>
+    //               {showPassword ? <ViewOffIcon /> : <ViewIcon />}
+    //             </Button>
+    //           </InputRightElement>
+    //         </InputGroup>
+    //       </FormControl>
+    //       <Button colorScheme="blue" type="submit" width="100%">
+    //         Register
+    //       </Button>
+    //     </form>
+    //     <Text onClick={() => navigate('/login')}>Sudah punya akun? Login</Text>
+    //   </Box>
+    // </Flex>
   );
 };
 
